0f77aded14ee8f921f03a012dd77dcdcdbac791e
iambetelhem24 <betelhemlegesse16@gmail.com>
2026-01-21 19:47:00 +0200

    Feat: initialize Express server with basic middleware

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('FamEmergency Server is Running!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
