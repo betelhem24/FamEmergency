454c3a648552326b5c68097af879da0f779e19de
iambetelhem24 <betelhemlegesse16@gmail.com>
2026-01-25 12:44:36 +0200

    feat: implement React Router navigation with Register and Login routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux'; // The "Plug"
-import { store } from './store';        // The "Brain"
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; // New Tools
+import { store } from './store';
 import App from './App';
+import Login from './Login'; // Import the placeholder we made
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/*  I wrap App in Provider so every component can access the Brain */}
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          {/*  When URL is /register, show the App component */}
+          <Route path="/register" element={<App />} />
+          
+          {/*  When URL is /login, show the Login component */}
+          <Route path="/login" element={<Login />} />
+          
+          {/*  If someone goes to just "/", send them to /login automatically */}
+          <Route path="/" element={<Navigate to="/login" />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
